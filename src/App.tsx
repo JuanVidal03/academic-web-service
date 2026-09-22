c400055b570917f15a716130087a76f393129c3a
Juan Vidal <juanmvg2003@gmail.com>
2026-09-22 16:43:42 -0500

    feat: wrap Routes with Suspense component and add loading fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
