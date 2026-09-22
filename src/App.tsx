f25c30add5fbeac130de6d6aa9cdbe895bc84687
JesusPaladinez <jesuspaladinez18@gmail.com>
2026-09-22 16:52:06 -0500

    feat: implement lazy loading for Login and Dashboard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+  
+const Login = lazy(() => import("@/pages/Login"));
+const Dashboard = lazy(() => import("@/pages/Dashboard"));
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <Routes>
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
